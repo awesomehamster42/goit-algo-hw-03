e5a81f082e883ed6c556232004eaabb8cb24ffe1
awesomehamster42 <awesomehamster.work@gmail.com>
2024-09-30 14:11:33 +0300

    change quantity condition

--- hw-03-02.py
+++ hw-03-02.py
@@ -4,7 +4,7 @@ import random
 def get_numbers_ticket(min, max, quantity):
 
     # Перевірка на відповідність умовам
-    if min >= 1 and max <= 1000 and min < quantity < max:
+    if min >= 1 and max <= 1000:
 
         # Створюємо список чисел від min до max
         numbers = list(range(min, max+1))
@@ -18,3 +18,7 @@ def get_numbers_ticket(min, max, quantity):
 
         # Повертаємо пустий список чисел, якщо умови не виконуються
         return []
+    
+
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
